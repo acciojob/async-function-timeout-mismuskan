c4eba8d2264be4019f9751348d4c5b15e2aa9856
Muskan Sheikh <144351188+mismuskan@users.noreply.github.com>
2024-01-31 23:48:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,13 +4,10 @@ async function delayAndShowMessage() {
   const textInput = document.getElementById('text').value;
   const delayInput = document.getElementById('delay').value;
 
-  // Convert delay to milliseconds
   const delayInMilliseconds = delayInput * 1000;
 
-  // Wait for the specified delay
   await new Promise(resolve => setTimeout(resolve, delayInMilliseconds));
 
-  // Display the message on the webpage
   document.getElementById('output').innerText = textInput;
 }
 document.getElementById('btn').addEventListener('click', delayAndShowMessage);
